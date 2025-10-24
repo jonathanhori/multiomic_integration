7946b10f066f17c8fb33ed0e0cd30e48d6ed5cd7
Jonathan Hori <jhori333@gmail.com>
2025-10-24 10:15:52 -0700

    Matrix decomp working example

--- python/matrix_decomp.py
+++ python/matrix_decomp.py
@@ -9,7 +9,7 @@ import pyro
 import pyro.distributions as dist
 
 from pyro.optim import Adam
-from pyro.infer import SVI, Trace_ELBO
+from pyro.infer import SVI, Trace_ELBO, Predictive
 from pyro.infer.autoguide import AutoContinuous, AutoNormal #AutoMultivariateNormal
 
 from torch.utils.data import TensorDataset, DataLoader
@@ -32,9 +32,14 @@ def bayesian_factor_model(X, # Input
     X_i ~ N_p(Lambda @ Z_i, diag(psi))
     """
     # X is TensorDataset
-    # m = len(X)
-    # p = X[0][0].shape[0]
-    m, p = X.shape # Working with minibatches of size m
+    
+    if torch.is_tensor(X):
+        m, p = X.shape # Working with minibatches of size m
+    elif isinstance(X, TensorDataset): # working with full dataset directly
+        print("is tensordataset")
+        m = len(X)
+        p = X[0][0].shape[0]
+        
     
     ########################
     # ---- Loadings --------
@@ -95,18 +100,24 @@ def bayesian_factor_guide(X, k, n):
 
 def train_decomp(X, k, 
                  epochs = 20,
+                 max_iter = 20000,
+                 minibatch_flag = False,
                  minibatch_size = 32,
-                 tol = 1e-3,
+                 tol = 1e-4,
+                 opt_args = {"lr": 0.001},
                  device = "cpu"):
     # X = X.to(device)
     n, p = X.shape
     
+    # if minibatch_flag == False, then do not minibatch, data loader not necessary
+    if not minibatch_flag:
+        minibatch_size = n
     
     ########################
     # Handle minibatching of dataset
     ########################
     X_mod = TensorDataset(X)
-    loader = DataLoader(X_mod, batch_size = minibatch_size, shuffle = True)
+    loader = DataLoader(X_mod, batch_size = minibatch_size, shuffle = True, drop_last=True)
     
     ########################
     # CURRENTLY: model takes multiple arguments. User helper functions to load data and arguments 
@@ -131,7 +142,7 @@ def train_decomp(X, k,
     # mod = partial(bayesian_factor_model, k = k, n = n)
     # guide = guide_batch(mod)
     # guide = bayesian_factor_guide()
-    opt = Adam({"lr": 0.001})
+    opt = Adam(opt_args)
     elbo = Trace_ELBO()
     # svi = SVI(model_batch, guide_batch, opt, loss = elbo)
     svi = SVI(model_batch, guide, opt, loss = elbo)
@@ -140,36 +151,65 @@ def train_decomp(X, k,
     
     ########################
     # Train
+    # If minibatching: pass batch from loader
+    # If not minibatching: pass original data as tensor
     ########################
-    prev_loss = None
-    for epoch in range(epochs):
-        epoch_loss = 0.
-        for batch, in loader:
-            # print(batch.shape)
-            loss = svi.step(batch)
-            # print(loss)
-            epoch_loss += loss
-        print(f"Epoch {epoch+1}/{epochs}  avg neg-ELBO per datum: {epoch_loss / n:.4f}")
-        
-        if prev_loss is not None and abs(epoch_loss - prev_loss) < tol:
-            break
+    if minibatch_flag:
+        prev_loss = None
+        for epoch in range(epochs):
+            epoch_loss = 0.
+            for batch, in loader:
+                # print(batch.shape)
+                loss = svi.step(batch)
+                # print(loss)
+                epoch_loss += loss
+            print(f"Epoch {epoch+1}/{epochs}  avg neg-ELBO per datum: {epoch_loss / n:.4f}")
+            print(f"Loss at epoch {epoch+1}: {loss / n}")
             
-        # print(f"delta: {epoch_loss / n - (prev_loss / n if prev_loss is not None else epoch_loss / n):+.6f}")
-        
-        # for name in pyro.get_param_store().get_all_param_names():
-        #     p = pyro.param(name)
-        #     print(name, p.shape, getattr(p, "grad", None) is not None)
-        
-        prev_loss = loss
-    # for epoch in range(epochs):
-    #     epoch_loss = 0.
-    #     loss = svi.step(X_mod) #batch[0])
-    #     print(loss)
-    #     epoch_loss += loss
-    #     # for batch in loader:
-    #     #     loss = svi.step(batch[0])
-    #     #     epoch_loss += loss
-    #     print(f"Epoch {epoch+1}/{epochs}  avg neg-ELBO per datum: {epoch_loss:.4f}")
+            if prev_loss is not None and abs(epoch_loss - prev_loss) < tol:
+                break
+                
+            # print(f"delta: {epoch_loss / n - (prev_loss / n if prev_loss is not None else epoch_loss / n):+.6f}")
+            
+            # for name in pyro.get_param_store().get_all_param_names():
+            #     p = pyro.param(name)
+            #     print(name, p.shape, getattr(p, "grad", None) is not None)
+            
+            prev_loss = loss
+    else:
+        # total_loss = 0.
+        prev_loss = None
+        for iter in range(max_iter):
+            # total_loss = 0.
+            loss = svi.step(X) #batch[0])
+            # print(loss)
+            # total_loss += loss
+            # for batch in loader:
+            #     loss = svi.step(batch[0])
+            #     epoch_loss += loss
+            if iter % 100 == 0:
+                print(f"Iteration {iter}  loss: {loss / n:.4f}")
+                # print(f"Epoch {iter+1}/{epochs}  avg neg-ELBO per datum: {epoch_loss:.4f}")
+                
+            # if prev_loss is not None and abs((loss- prev_loss) / prev_loss) < tol:
+            if prev_loss is not None and abs((loss- prev_loss)) < tol:
+                # print(f"Stopping: {abs((loss- prev_loss) / prev_loss)} < {tol}")
+                print(f"Iteration {iter-1}  loss: {prev_loss / n:.4f}")
+                print(f"Iteration {iter}  loss: {loss / n:.4f}")
+                break
+            prev_loss = loss
         
     return svi#, guide
     # return svi, guide
+    
+def predict_factor_model(model,
+                         guide,
+                         num_samples,
+                         data):
+    
+    num_samples = 1000
+    predictive = Predictive(model, guide=guide, num_samples=num_samples)
+    return predictive(**data)
+    # svi_samples = {k: v.reshape(num_samples).detach().cpu().numpy()
+    #             for k, v in predictive(data).items()
+    #             if k != "obs"}
