aa8b0ee7c10748ccece1c6dc2ccf10c4c767cae7
Jonathan Hori <jhori333@gmail.com>
2025-10-27 19:16:26 -0700

    Forward method for SupMultiviewDecomp implemented

--- python/model.py
+++ python/model.py
@@ -19,34 +19,59 @@ pyro.enable_validation(True)
 pyro.set_rng_seed(0)
 
 
-class FactorModelARD(PyroModule):
-    def __init__(self, k, n,
-                 a_sigma=2.0, # Hyperparams: ARD prior per factor: IG
-                 b_sigma=2.0,
-                 a_psi=2.0, # Hyperparams: per-view error: IG
-                 b_psi=2.0):
+class SupMultiviewDecomp(PyroModule):
+    def __init__(self, 
+                 k, 
+                 k_l_list,
+                 n,
+                 a_sigma_joint=2.0, # Hyperparams: ARD prior per factor: IG same for all views
+                 b_sigma_joint=2.0,
+                 a_sigma_view=2.0, # Hyperparams: ARD prior per factor: IG same for all views
+                 b_sigma_view=2.0,
+                 a_psi=2.0, # Hyperparams: per-view error: IG same for all views
+                 b_psi=2.0,
+                 a_sigma_y = 2.0, # Hyperparams: outcome error: IG
+                 b_sigma_y = 2.0,
+                 a_sigma_beta = 2.0, # Hyperparams: outcome coefficients: IG
+                 b_sigma_beta = 2.0
+                 ):
         super().__init__()
         # Model parameters
         self.k = k
+        self.k_l = k_l_list # assumes k_l > 0 for all l
         self.n = n
-        self.p = None
+        self.p_l = None
         
         # Hyperparams
-        self.a_sigma = a_sigma
-        self.b_sigma = b_sigma
+        self.a_sigma_joint = a_sigma_joint
+        self.b_sigma_joint = b_sigma_joint
+        self.a_sigma_view = a_sigma_view
+        self.b_sigma_view = b_sigma_view
         self.a_psi = a_psi
         self.b_psi = b_psi
+        self.a_sigma_y = a_sigma_y
+        self.b_sigma_y = b_sigma_y
+        self.a_sigma_beta = a_sigma_beta,
+        self.b_sigma_beta = b_sigma_beta
         
         self.loss_history = []
         
     
-    def forward(self, X, batch_idx):
-        if torch.is_tensor(X):
-            m, p = X.shape # Working with minibatches of size m
-        elif isinstance(X, TensorDataset): # working with full dataset directly
-            # print("is tensordataset")
-            m = len(X)
-            p = X[0][0].shape[0]
+    def forward(self, 
+                X_list, 
+                batch_idx,
+                y = None):
+        """
+        X_l = Z @ Lambda_l^T + Phi_l @ Gamma_l^T + E
+        """
+        m = len(batch_idx)
+        p_l_list = [X_l.shape[1] for X_l in X_list]
+        # if torch.is_tensor(X):
+        #     m, p = X.shape # Working with minibatches of size m
+        # elif isinstance(X, TensorDataset): # working with full dataset directly
+        #     # print("is tensordataset")
+        #     m = len(X)
+        #     p = X[0][0].shape[0]
         # self.p = 
         
         ########################
@@ -56,13 +81,33 @@ class FactorModelARD(PyroModule):
         # Lambda_j ~ N_k(0, sigma_k^2 I)
         #   ==> Lambda_jk ~ N(0, sigma_k^2)
         # sigma_k^2 ~ InvGamma(a_sigma, b_sigma))
+        #
+        # Gamma_jk ~ N(0, )
         ########################
-        # No row-wise conditional independence of loadings.
-        sigma2 = pyro.sample("sigma2_lambda", dist.InverseGamma(self.a_sigma, self.b_sigma).expand([self.k]).to_event(1))
-        sigma = torch.sqrt(sigma2)
+        # VIEW SPECIFIC
+        Gamma_l_list = []
+        for l, (p_l, k_l) in enumerate(zip(p_l_list, self.k_l_list)):
+            sigma2_gamma_l = pyro.sample(f"sigma2_gamma_l{l}", 
+                                   dist.InverseGamma(self.a_sigma_view, self.b_sigma_view).expand([k_l]).to_event(1))
+            sigma_gamma_l = torch.sqrt(sigma2_gamma_l)
         
-        # sigma is broadcast across rows of Lambda
-        Lambda = pyro.sample("Lambda", dist.Normal(torch.zeros(p, self.k), sigma).to_event(2))
+            # sigma is broadcast across rows of Lambda
+            Gamma_l = pyro.sample(f"Gamma_l{l}", dist.Normal(torch.zeros(p_l, k_l), sigma_gamma_l).to_event(2))
+            
+            Gamma_l_list.append(Gamma_l)
+        
+        # SHARED
+        Lambda_l_list = []
+        for l, p_l in enumerate(p_l_list, self.k_l_list):
+            sigma2_lambda_l = pyro.sample(f"sigma2_lambda_l{l}", 
+                                   dist.InverseGamma(self.a_sigma_joint, self.b_sigma_joint).expand([self.k]).to_event(1))
+            sigma_lambda_l = torch.sqrt(sigma2_lambda_l)
+        
+            # sigma is broadcast across rows of Lambda
+            Lambda_l = pyro.sample(f"Lambda_l{l}", dist.Normal(torch.zeros(p_l, self.k), sigma_lambda_l).to_event(2))
+            
+            Lambda_l_list.append(Lambda_l)
+        
         
         ########################
         # ---- Observations --------
@@ -72,9 +117,23 @@ class FactorModelARD(PyroModule):
         #   function to perform minibatching. 
         ########################
         
-        # Idiosyncratic error variance            
-        psi = pyro.sample("psi", dist.InverseGamma(self.a_psi, self.b_psi).expand([p]).to_event(1))
-        psi_sqrt = torch.sqrt(psi)
+        # Idiosyncratic error variance 
+        psi_sqrt_l_list = []           
+        for l, p_l in enumerate(p_l_list):
+            psi_l = pyro.sample(f"psi_l{l}", dist.InverseGamma(self.a_psi, self.b_psi).expand([p_l]).to_event(1))
+            psi_sqrt = torch.sqrt(psi_l)
+            psi_sqrt_l_list.append(psi_sqrt)
+            
+        # Outcome variances
+        sigma2_y = pyro.sample("sigma2_y",
+                               dist.InverseGamma(self.a_sigma_y, self.b_sigma_y).to_event(1))
+        sigma_y = torch.sqrt(sigma2_y)
+        
+        # Outcome model coefficients
+        sigma2_beta = pyro.sample("sigma2_beta",
+                                  dist.InverseGamma(self.a_sigma_beta, self.b_sigma_beta).expand([self.k]).to_event(1))
+        beta = pyro.sample("beta",
+                           dist.Normal(torch.zeros(self.k), sigma2_beta).to_event(1))
         
         # Local latent variables and observations
         with pyro.plate("obs", self.n, subsample = batch_idx): # X is a minibatch, can pass directly into subsample
@@ -82,10 +141,38 @@ class FactorModelARD(PyroModule):
             # Z_batch = pyro.sample("Z", dist.Normal(torch.zeros(m, self.k), torch.ones(self.k)).to_event(1))
             Z_batch = pyro.sample("Z", dist.Normal(0., 1.).expand([self.k]).to_event(1))
             
-            # Compute structure
-            structure = pyro.deterministic("structure", torch.matmul(Z_batch, Lambda.T))
+            Phi_l_list = []
+            for l, k_l in enumerate(self.k_l):
+                Phi_l = pyro.sample(f"Phi_l{l}", dist.Normal(0., 1.).expand([k_l]).to_event(1))
+                Phi_l_list.append(Phi_l)
+                
             
-            pyro.sample("X", dist.Normal(structure, psi_sqrt).to_event(1), obs = X)
+            # Compute structure
+            joint_structure_list = []
+            for l in range(len(X_list)):
+                joint_structure_l = pyro.deterministic(f"joint_structure_l{l}", 
+                                                       torch.matmul(Z_batch, Lambda_l_list[l].T))
+                view_structure_l = pyro.deterministic(f"view_structure_l{l}",
+                                                    torch.matmul(Phi_l_list[l], Gamma_l_list[l].T))
+                
+                total_structure_l = torch.add(joint_structure_l, view_structure_l)
+                
+                joint_structure_list.append(joint_structure_l)
+            
+                pyro.sample(f"X_l{l}", dist.Normal(total_structure_l, 
+                                                   psi_sqrt_l_list[l]).to_event(1), 
+                            obs = X_list[l])
+            
+            outcome_structure = torch.matmul(Z_batch, beta)
+                
+            pyro.sample("y", dist.Normal(outcome_structure, sigma_y),
+                        obs = y)
+            
+            
+        ########################
+        # ---- Outcome --------
+        # Dependent on just shared factors, or all factors
+        ########################
             
             
     def guide(self, X, batch_idx):
@@ -143,14 +230,15 @@ class FactorModelARD(PyroModule):
             
     
 ##############
-def do_inference(X, model, guide, opt = "Adam",
-                    epochs = 20,
-                    max_iter = 20000,
-                    minibatch_flag = False,
-                    minibatch_size = 32,
-                    tol = 1e-4,
-                    opt_args = {"lr": 0.001},
-                    device = "cpu"):
+def do_inference(X, model, guide, 
+                 opt = "Adam",
+                epochs = 20,
+                max_iter = 20000,
+                minibatch_flag = False,
+                minibatch_size = 32,
+                tol = 1e-4,
+                opt_args = {"lr": 0.001},
+                device = "cpu"):
     
     # if minibatch_flag == False, then do not minibatch, data loader not necessary
     if not minibatch_flag:
