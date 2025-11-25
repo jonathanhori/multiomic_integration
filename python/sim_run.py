59e80f9b2d38f19b81e4d1060a0a431954fb7960
Jonathan Hori <jhori333@gmail.com>
2025-11-25 13:26:37 -0800

    Accomodating train test split, debugging

--- python/sim_run.py
+++ python/sim_run.py
@@ -12,9 +12,12 @@ import itertools
 # from rpy2.robjects import pandas2ri, default_converter, conversion
 
 import torch
+from torch.utils.data import TensorDataset, random_split
 import pyro
 from pyro.optim import Adam, ClippedAdam
 from pyro.infer import Trace_ELBO
+
+from sklearn.model_selection import train_test_split
 
 # import importlib
 
@@ -42,13 +45,17 @@ dirs = [os.path.join(sim_data_path, dir) \
 
 filename_dict = {os.path.basename(name): os.listdir(os.path.expanduser(name)) for name in dirs}
 
+RANDOM_SEED = 123
+TRAINING_SPLIT = True
+TRAINING_SIZE = 0.8
 
 n_array = (50, 100, 500) #, 1000)
 p_array = (50, 100, 1000)
 snr_x_array = [2] #(2, 1, 0.5)
 snr_y_array = [2] #(2, 1, 0.5)
 reps = 10
-loading_sparsity = 0.5
+loading_sparsity = [0, 0.25, 0.5]
+k_deltas = [0, 10]
 
 sim_grid = itertools.product(
     n_array,
@@ -56,13 +63,16 @@ sim_grid = itertools.product(
     snr_x_array,
     snr_y_array,
     # range(reps),
-    [loading_sparsity]
+    loading_sparsity,
+    k_deltas
+    # [loading_sparsity]
 )
 
 N_POSTERIOR_SAMPLES = 1000
 TOL = 1
 VARIATIONAL_TOL = 0.3
 MINIBATCH_SIZE = 32
+MIN_EPOCHS = 20
 NUM_EPOCHS = 1000
 initial_lr = 0.005
 
@@ -72,11 +82,11 @@ lrd = gamma ** (1 / (NUM_EPOCHS * MINIBATCH_SIZE))
 file_dir_base = "n{}p{}_snr{}.{}_sparse{}" #/sim_data_ywithview_rep{}.rds"
 file_name_base = "sim_data_ywithview_rep{}"
 
-model_out_path = os.path.expanduser(f"~/Library/Mobile Documents/com~apple~CloudDocs/Projects/multiomic_integration/sim/results/integration/sparse{loading_sparsity}/models")
-model_out_filename_base = "run_autonormalguide_n{}p{}_snr{}.{}_sparse{}_rep{}"
+model_out_path = os.path.expanduser("~/Library/Mobile Documents/com~apple~CloudDocs/Projects/multiomic_integration/sim/results/integration/models")
+model_out_filename_base = "run_autonormalguide_n{}p{}_snr{}.{}_sparse{}_deltak{}_rep{}"
 
-metric_out_path = os.path.expanduser(f"~/Library/Mobile Documents/com~apple~CloudDocs/Projects/multiomic_integration/sim/results/integration/sparse{loading_sparsity}/metrics")
-metric_out_filename_base = "run_autonormalguide_n{}p{}_snr{}.{}_sparse{}_rep{}"
+metric_out_path = os.path.expanduser("~/Library/Mobile Documents/com~apple~CloudDocs/Projects/multiomic_integration/sim/results/integration/metrics")
+metric_out_filename_base = "run_autonormalguide_n{}p{}_snr{}.{}_sparse{}_deltak{}_rep{}"
 
 # Create output paths if don't exist
 Path(model_out_path).mkdir(parents=True, exist_ok=True)
@@ -85,7 +95,7 @@ Path(metric_out_path).mkdir(parents=True, exist_ok=True)
 metric_list = []
 
 if __name__ == "__main__":
-    for n, p, snr_x, snr_y, sparsity in sim_grid:
+    for n, p, snr_x, snr_y, sparsity, k_delta in sim_grid:
         pyro.clear_param_store()
         
         print("Loading:")
@@ -94,7 +104,7 @@ if __name__ == "__main__":
         # Determine if all reps are already done - skip training
         check_model_out_filename = os.path.join(
             model_out_path, 
-            model_out_filename_base.format(n, p, snr_x, snr_y, sparsity, reps))
+            model_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, reps))
         if os.path.isfile(check_model_out_filename + ".pth"):
             print('Model already trained, moving on')
             continue
@@ -116,7 +126,7 @@ if __name__ == "__main__":
             # Determine if model is already saved - skip training
             model_out_filename = os.path.join(
                 model_out_path, 
-                model_out_filename_base.format(n, p, snr_x, snr_y, sparsity, rep))
+                model_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, rep))
             if os.path.isfile(model_out_filename + ".pth"):
                 print('Model already trained, moving on')
                 continue
@@ -127,27 +137,71 @@ if __name__ == "__main__":
             sim_data = files_for_condition.get(file_name_base.format(rep))
             
             L = int(sim_data.get("L"))
-            k = int(sim_data.get("K"))
             X_l_list = sim_data.get("X_l")
             y = sim_data.get("y")
-            k_l_list = sim_data.get("K_l").int().tolist()
+            k = int(sim_data.get("K") + k_delta)
+            k_l_list = [int(k_l + k_delta) for k_l in sim_data.get("K_l").int().tolist()]
             n = X_l_list[0].shape[0]
             p_l = int(sim_data.get("p_l")[0])
+            
+            # Perform train/test split if necessary
+            if TRAINING_SPLIT:
+                train_idx, test_idx = train_test_split(
+                    torch.arange(X_l_list[0].shape[0]),
+                    train_size = TRAINING_SIZE,
+                    random_state = RANDOM_SEED,
+                    shuffle = True
+                )
+                train_X_l_list = [X[train_idx, :] for X in X_l_list]
+                test_X_l_list = [X[test_idx, :] for X in X_l_list]
+                
+                train_y = y[train_idx]
+                test_y = y[test_idx]
+            else:
+                train_idx = torch.arange(n)
+                test_idx = torch.arange(n)
+                
+                train_X_l_list = X_l_list
+                test_X_l_list = train_X_l_list
+                
+                train_y = y
+                test_y = train_y
 
-            # Clean: remove 0 variance columns and standardize            
-            X_l_list_column_filters = [zero_variance_col_filter(X) for X in X_l_list]
-            X_l_list = [X[:, col_filter] for X, col_filter in zip(X_l_list, X_l_list_column_filters)]
+            # Clean: remove 0 variance columns and standardize. Using Training data            
+            X_l_list_column_filters = [zero_variance_col_filter(X) for X in train_X_l_list]
+            
+            train_X_l_list = [X[:, col_filter] for X, col_filter in zip(train_X_l_list, X_l_list_column_filters)]
+            test_X_l_list = [X[:, col_filter] for X, col_filter in zip(test_X_l_list, X_l_list_column_filters)]
 
-            clean_X_dict_list = list(map(normalize_tensor_by_col, X_l_list))
-        
-            X_l_list_clean = [clean["data_clean"] for clean in clean_X_dict_list]
-            X_l_mean_list = [clean["means"] for clean in clean_X_dict_list]
-            X_l_sd_list = [clean["sds"] for clean in clean_X_dict_list]
+            clean_train_X_dict_list = [normalize_tensor_by_col(X) for X in train_X_l_list]
+            X_l_mean_list = [clean["means"] for clean in clean_train_X_dict_list]
+            X_l_sd_list = [clean["sds"] for clean in clean_train_X_dict_list]
+            
+            clean_test_X_dict_list = [normalize_tensor_by_col(X, means, stds) 
+                                      for X, means, stds in 
+                                      zip(test_X_l_list, X_l_mean_list, X_l_sd_list)]
+            
+            train_X_l_list_clean = [clean["data_clean"] for clean in clean_train_X_dict_list]
+            test_X_l_list_clean = [clean["data_clean"] for clean in clean_test_X_dict_list]
 
-            y_mean = torch.mean(y)
-            y_std = torch.std(y)
-            y_clean = (y - y_mean) / y_std
-
+            y_mean = torch.mean(train_y)
+            y_std = torch.std(train_y)
+            
+            train_y_clean = (train_y - y_mean) / y_std
+            test_y_clean = (test_y - y_mean) / y_std
+            
+            
+            # Package data into dataset and 
+            train_subset = TensorDataset(torch.arange(train_X_l_list_clean[0].shape[0]), 
+                                        *train_X_l_list_clean, 
+                                        train_y_clean)
+            # if TRAINING_SPLIT:
+            #     train_subset, test_subset = random_split(tensor_dataset, 
+            #                                             (0.8, 0.2), 
+            #                                             generator = torch.Generator().manual_seed(RANDOM_SEED))
+            # else:
+            #     train_subset = tensor_dataset
+                
             ##################
             # Perform model inference
             factor_model = SupMultiviewDecomp(
@@ -155,7 +209,7 @@ if __name__ == "__main__":
                 k_l_list,
                 n,
                 include_view_factors = True,
-                dense = True
+                dense = False
             )
 
             def subsample_create_plates(X, batch_idx, y = None):
@@ -170,14 +224,16 @@ if __name__ == "__main__":
             try:
                 t0 = time.time()
                 svi, opt = do_inference(
-                    X_l_list_clean,
-                    y_clean,
+                    # X_l_list_clean,
+                    # y_clean,
+                    train_subset,
                     model = factor_model,
                     guide = guide,
                     opt = OPT,
                     elbo = LOSS,
                     # tol = TOL, # tolerance is on epoch loss per datum,
                     variational_tol = VARIATIONAL_TOL,
+                    min_epochs = MIN_EPOCHS,
                     epochs = NUM_EPOCHS,
                     minibatch_flag = True,
                     minibatch_size = MINIBATCH_SIZE,
@@ -205,7 +261,11 @@ if __name__ == "__main__":
 
                 pyro.get_param_store().save(model_out_filename + "_paramstore.pth") #os.path.join(model_out_path, model_out_filename + "_paramstore.pth"))
                 
-                
+                # if TRAINING_SPLIT:
+                #     print()
+                # else:
+                #     print()
+                    
                 #################
                 # Sample from posterior
                 print('Sampling from posterior')
@@ -214,23 +274,31 @@ if __name__ == "__main__":
                 post_samples, post_samples_y = obtain_posterior_pred_samples(factor_model,
                                                                             guide,
                                                                             N_POSTERIOR_SAMPLES,
-                                                                            X_l_list_clean,
+                                                                            train_X_l_list_clean,
+                                                                            test_X_l_list_clean,
                                                                             sites,
                                                                             outcome_sites)
                     
                 
                 #################
                 # Calculate simulated and posterior sample structures
+                # Compare structure decomposition in training data with prediction performance
+                #       in test data
                 SIM_decomp = extract_sim_decomp(sim_data)
+                
+                # Filter corresponding rows of test set structures
+                train_SIM_decomp = {k: v for k, v in SIM_decomp.items()}
+                train_SIM_decomp['SIM_Z'] = train_SIM_decomp['SIM_Z'][train_idx, :]
+                train_SIM_decomp['SIM_Phi_l_list'] = [Phi[train_idx, :] for Phi in train_SIM_decomp['SIM_Phi_l_list']]
             
                 eval_structures = calc_all_structures_with_rescaling(L,
-                                                                     X_l_list_column_filters,
+                                                                    X_l_list_column_filters,
                                                                     X_l_mean_list,
                                                                     X_l_sd_list,
                                                                     post_samples,
                                                                     post_samples_y,
-                                                                    SIM_decomp,
-                                                                    y_clean.squeeze())
+                                                                    train_SIM_decomp,
+                                                                    test_y_clean.squeeze())
                     
 
                 #################
@@ -245,14 +313,15 @@ if __name__ == "__main__":
                         snr_x = snr_x,
                         snr_y = snr_y,
                         rep = rep,
-                        sparsity = loading_sparsity,
+                        sparsity = sparsity,
+                        k = k,
                         runtime = run_minibatch
                         )
 
                 # Save evaluation
                 metric_out_filename = os.path.join(
                     metric_out_path, 
-                    metric_out_filename_base.format(n, p, snr_x, snr_y, sparsity, rep))
+                    metric_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, rep))
                 print("Exporting metrics:")
                 print(metric_out_filename)
 
