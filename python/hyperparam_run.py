9cd4cec0daabac90f45730fd7d3c5eca437543be
Jonathan Hori <jhori333@gmail.com>
2026-04-23 10:52:17 -0700

    Updates to hyperparameter search for integration model

--- python/hyperparam_run.py
+++ python/hyperparam_run.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import pandas as pd
 
 import itertools
 
@@ -84,11 +85,11 @@ N_POSTERIOR_SAMPLES = 500
 
 lr_array = [0.1, 0.05, 0.01]
 beta_array = [(0.9, 0.999), (0.8, 0.999), (0.9, 0.9)]
-particle_array = [1]
+particle_array = [2]
 lr_step_array = [5, 10, 20, 30]
 lr_decay_array = [0.9, 0.5, 0.1]
 minibatch_array = [16, 32, 64]
-min_epoch_array = [20]
+min_epoch_array = [50]
 max_epoch_array = [300]
 
 train_grid = itertools.product(
@@ -104,41 +105,42 @@ train_grid = itertools.product(
 train_grid_names = ('initial_lr', 'betas', 'num_particles', 'lr_step_size', \
     'lr_decay_factor', 'minibatch_size', 'min_epochs', 'max_epochs')
 
-metric_list = []
+# metric_list = []
+metric_table_concat = pd.DataFrame()
 
 if __name__ == "__main__":
     for n, p, snr_x, snr_y, sparsity, k_delta in sim_grid:
+        # Load data
+        print("Loading:")
+        print(file_dir_base.format(n, p, snr_x, snr_y, sparsity))
+        # file_name_base.format(n, p, snr_x, snr_y, sparsity) #, rep)
+        files_for_condition = load_and_process_rds_data_for_condition(
+            file_dir_base.format(n, p, snr_x, snr_y, sparsity), # "n100p100_snr2.2_sparse0", 
+            train_sim_data_path, # loaded above
+            filename_dict, # loaded above
+            reps) # how many reps to import
+        if not TRAINING_SPLIT:
+            test_files_for_condition = load_and_process_rds_data_for_condition(
+                file_dir_base.format(n, p, snr_x, snr_y, sparsity), # "n100p100_snr2.2_sparse0", 
+                test_sim_data_path, # loaded above
+                test_filename_dict, # loaded above
+                reps) # how many reps to import
+        else:
+            test_files_for_condition = None
+            
+            
         for params in train_grid:
             train_config = dict(zip(train_grid_names, params))
             
-            pyro.clear_param_store()
+            pyro.clear_param_store()          
             
-            print("Loading:")
-            print(file_dir_base.format(n, p, snr_x, snr_y, sparsity))
-            
-            # Determine if all reps are already done - skip training
-            check_metric_out_filename = os.path.join(
-                metric_out_path, 
-                metric_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, reps))
-            if os.path.isfile(check_metric_out_filename + ".pth"):
-                print('Metrics already evaluated for model, moving on')
-                continue
-            
-            # Load data
-            # file_name_base.format(n, p, snr_x, snr_y, sparsity) #, rep)
-            files_for_condition = load_and_process_rds_data_for_condition(
-                file_dir_base.format(n, p, snr_x, snr_y, sparsity), # "n100p100_snr2.2_sparse0", 
-                train_sim_data_path, # loaded above
-                filename_dict, # loaded above
-                reps) # how many reps to import
-            if not TRAINING_SPLIT:
-                test_files_for_condition = load_and_process_rds_data_for_condition(
-                    file_dir_base.format(n, p, snr_x, snr_y, sparsity), # "n100p100_snr2.2_sparse0", 
-                    test_sim_data_path, # loaded above
-                    test_filename_dict, # loaded above
-                    reps) # how many reps to import
-            else:
-                test_files_for_condition = None
+            # # Determine if all reps are already done - skip training
+            # check_metric_out_filename = os.path.join(
+            #     metric_out_path, 
+            #     metric_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, reps))
+            # if os.path.isfile(check_metric_out_filename + ".pth"):
+            #     print('Metrics already evaluated for model, moving on')
+            #     continue
             
             # print("Files for sim configuration:")
             # print(files_for_condition.keys())
@@ -159,8 +161,15 @@ if __name__ == "__main__":
                 # Determine if model is already saved - skip training
                 model_out_filename = os.path.join(
                     model_out_path, 
-                    model_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, rep))
-                if os.path.isfile(model_out_filename + ".pth"):
+                    model_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, rep) + "_" \
+                        + ".".join(f'{key}{val}' for key, val in train_config.items())
+                        )
+                check_metric_out_filename = os.path.join(
+                    metric_out_path, 
+                    metric_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, rep) + "_" \
+                        + ".".join(f'{key}{val}' for key, val in train_config.items())
+                        )
+                if os.path.isfile(check_metric_out_filename + ".csv"):
                     print('Model already trained, moving on')
                     continue
                 
@@ -189,17 +198,18 @@ if __name__ == "__main__":
                 }
                 local_config = train_config
                 
-                factor_model, train_handler, global_train_state = train_model(model_config,
-                                                        train_config,
-                                                        train_subset,
-                                                        model_out_filename)
-                
-                factor_model, test_handler, local_train_state = train_model_locally(factor_model,
-                                                                local_config,
-                                                                test_subset,
-                                                                model_out_filename)
-                
-                try:             
+                try:
+                    factor_model, train_handler, global_train_state = train_model(model_config,
+                                                            train_config,
+                                                            train_subset,
+                                                            model_out_filename)
+                    
+                    factor_model, test_handler, local_train_state = train_model_locally(factor_model,
+                                                                    local_config,
+                                                                    test_subset,
+                                                                    model_out_filename)
+                    
+                                
                     metric_out_filename = os.path.join(
                         metric_out_path, 
                         metric_out_filename_base.format(n, p, snr_x, snr_y, sparsity, k_delta, rep)
@@ -226,8 +236,13 @@ if __name__ == "__main__":
                     print("Exporting metrics:")
                     print(metric_out_filename)
 
-                    eval_metric_table.to_csv(metric_out_filename + ".csv")
+                    eval_metric_table.to_csv(metric_out_filename + "_" \
+                        + ".".join(f'{key}{val}' for key, val in train_config.items()) \
+                        + ".csv")
                     
-                    metric_list.append(eval_metric_table)
+                    metric_table_concat = pd.concat([metric_table_concat, eval_metric_table])
+                    # metric_list.append(eval_metric_table)
                 except Exception as e:
                     print(e)
+    
+    metric_table_concat.to_csv(metric_out_filename + "_concat.csv")
