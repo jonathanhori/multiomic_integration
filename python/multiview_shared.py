3db16bbafabeb7e31adce2c1bba098ab4981abe2
Jonathan Hori <jhori333@gmail.com>
2026-05-19 15:01:04 -0700

    Adjusting prior hyperparameters for beta, matching initialization of guide to prior seems to help performance. But optimization possibly slower.

--- python/multiview_shared.py
+++ python/multiview_shared.py
@@ -37,8 +37,8 @@ class SupMultiviewShared(PyroModule):
                  b_psi=1.0,
                  a_sigma_y=3.0,
                  b_sigma_y=1.0,
-                 a_sigma_beta=2.0,
-                 b_sigma_beta=2.0,
+                 a_sigma_beta=3.0,
+                 b_sigma_beta=1.0,
                  joint_scores_init=None,
                  joint_loadings_list_init=None):
         super().__init__()
@@ -304,20 +304,24 @@ class SupMultiviewShared(PyroModule):
         # Idiosyncratic error variance
         for l, p_l in enumerate(self.p_l_list):
             a_psi_l = pyro.param(Params.a_psi_l.format(l=l),
-                                 torch.tensor(self.inv_gamma_init_param),
+                                 torch.tensor(self.a_psi),
+                                #  torch.tensor(self.inv_gamma_init_param),
                                  constraint=dist.constraints.positive)
             b_psi_l = pyro.param(Params.b_psi_l.format(l=l),
-                                 torch.tensor(self.init_param),
+                                 torch.tensor(self.b_psi),
+                                #  torch.tensor(self.init_param),
                                  constraint=dist.constraints.positive)
             pyro.sample(Sites.psi_l.format(l=l),
                         dist.InverseGamma(a_psi_l, b_psi_l).expand([p_l]).to_event(1))
 
         # Outcome model coefficients
         a_sigma_beta = pyro.param(Params.a_sigma_beta,
-                                  torch.tensor(self.inv_gamma_init_param),
+                                  torch.tensor(self.a_sigma_beta),
+                                #   torch.tensor(self.inv_gamma_init_param),
                                   constraint=dist.constraints.positive)
         b_sigma_beta = pyro.param(Params.b_sigma_beta,
-                                  torch.tensor(self.init_param),
+                                  torch.tensor(self.b_sigma_beta),
+                                #   torch.tensor(self.init_param),
                                   constraint=dist.constraints.positive)
         pyro.sample(Sites.sigma2_beta,
                     dist.InverseGamma(a_sigma_beta, b_sigma_beta).expand([self.k]).to_event(1))
@@ -331,10 +335,12 @@ class SupMultiviewShared(PyroModule):
 
         if self.outcome == "gaussian":
             a_sigma_y = pyro.param(Params.a_sigma_y,
-                                   torch.tensor(self.inv_gamma_init_param),
+                                   torch.tensor(self.a_sigma_y),
+                                #    torch.tensor(self.inv_gamma_init_param),
                                    constraint=dist.constraints.positive)
             b_sigma_y = pyro.param(Params.b_sigma_y,
-                                   torch.tensor(0.5),
+                                   torch.tensor(self.b_sigma_y),
+                                #    torch.tensor(0.5),
                                 #    torch.tensor(self.init_param),
                                    constraint=dist.constraints.positive)
             pyro.sample(Sites.sigma2_y,
